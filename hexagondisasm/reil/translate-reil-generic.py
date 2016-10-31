921148237bbf260d06a126041ce7a95f66fda3b3
Lucas Molas <lmolas@fundacionsadosky.org.ar>
2016-10-31 15:29:06 -0300

    BARF example to add Hexagon architecture: using Hexagon translator now.

--- hexagondisasm/reil/translate-reil-generic.py
+++ hexagondisasm/reil/translate-reil-generic.py
@@ -7,17 +7,36 @@ from barf.arch.x86.x86translator import X86Translator
 from barf.arch.arch import ARCH_X86_MODE_32
 from barf.barf import BARF
 
+from hexagontranslator import HexagonTranslator
+from arch import HexagonArchitectureInformation
+
+"""
+Original example modified to use the Hexagon Translator
+but still with the X86Disassembler, this is done just to
+test the Hexagon Translator API, which is returning
+empty REIL instructions for now.
+
+Next step is to use the Hexagon Disassembler.
+"""
+
 if __name__ == "__main__":
     #
     # Open file
     #
     try:
-        filename = sys.argv[1]
+
+        # filename = sys.argv[1]
+        filename = r"/sbin/gdisk" # x86 ELF
+        # filename = r"../data/factorial_example.elf"
 
         arch_mode = ARCH_X86_MODE_32
-        arch_info = X86ArchitectureInformation(arch_mode)
+        # arch_info = X86ArchitectureInformation(arch_mode)
+        arch_info = HexagonArchitectureInformation()
+
         disassembler = X86Disassembler(architecture_mode=arch_mode)
-        translator = X86Translator(architecture_mode=arch_mode)
+
+        # translator = X86Translator(architecture_mode=arch_mode)
+        translator = HexagonTranslator()
 
         barf = BARF(filename)
 
