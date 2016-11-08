72a5c2a31970cbdf906bcd8fb898e97bfcd527b1
Lucas Molas <lmolas@fundacionsadosky.org.ar>
2016-11-08 14:24:55 -0300

    BARF: translator class for hexagon arch.

---
+++ hexagondisasm/reil/hexagontranslator.py
@@ -0,0 +1,53 @@
+"""Hexagon to IR Translator.
+
+Defined from BARF's ``Translator`` API which requires that the ``translate``
+method be implemented.
+
+Todo:
+    *
+
+"""
+from barf.arch.translator import Translator
+from hexagondisasm.reil.parser import HexagonBehaviorParser
+from hexagondisasm.common import UnknownBehaviorException
+
+
+class HexagonTranslator(Translator):
+
+    def __init__(self):
+        pass
+
+    def translate(self, instruction):
+        """Return IR representation of an instruction.
+        """
+
+        parser = HexagonBehaviorParser(debug=False)
+
+        if instruction.template is None:
+            # TODO: In which cases the template is None?
+            return []
+
+        behavior = instruction.template.behavior
+
+        if behavior == '':
+            # No behavior available (probably it wasn't correctly parsed
+            # from the Hexagon Reference Manual).
+            return []
+
+        try:
+            print("Parsing: {:s}".format(behavior.strip()))
+            parsed = parser.parse(behavior)
+            for ri in parsed._instructions:
+                print(ri)
+            print("DONE!")
+        except UnknownBehaviorException as e:
+            # log.info("Unknown behavior instruction: {:s}".format(behavior))
+            pass
+
+
+        return []
+
+    def reset(self):
+        """Restart IR register name generator.
+        """
+        pass
